5d148ff6286776a89d7074626df7bf988a2d4fe1
jose6rojas <jose99rojas@hotmail.com>
2019-02-09 22:52:00 -0600

    Updates

---
+++ precache-manifest.394af3a0e674c87552b05c3430eff084.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CV_React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "dc6364b12735d0b3ce79",
+    "url": "/CV_React/static/css/main.28be853d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CV_React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "88e4434653a0ca64ed95",
+    "url": "/CV_React/static/js/1.88e44346.chunk.js"
+  },
+  {
+    "revision": "c3e5ec5acd7c47080b50",
+    "url": "/CV_React/static/js/runtime~main.c3e5ec5a.js"
+  },
+  {
+    "revision": "fd4a31cdefe0601c036536dcf90810ac",
+    "url": "/CV_React/static/media/jose.fd4a31cd.jpg"
+  },
+  {
+    "revision": "dc6364b12735d0b3ce79",
+    "url": "/CV_React/static/js/main.dc6364b1.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CV_React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CV_React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CV_React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "88e4434653a0ca64ed95",
+    "url": "/CV_React/static/css/1.8ec227e6.chunk.css"
+  },
+  {
+    "revision": "619d5a268788998bdd82ee4ce4b9b267",
+    "url": "/CV_React/index.html"
+  }
+];
